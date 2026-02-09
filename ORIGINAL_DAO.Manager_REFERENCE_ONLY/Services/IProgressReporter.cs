28a235aa75266f43db93496ab8a1c9d2ba6a0c48
gtpdev-net <149839821+gtpdev-net@users.noreply.github.com>
2026-02-09 02:19:09 +0000

    Removes reference project

--- ORIGINAL_DAO.Manager_REFERENCE_ONLY/Services/IProgressReporter.cs
+++
@@ -1,8 +0,0 @@
-namespace ORIGINAL_DAO.Manager_REFERENCE_ONLY.Services;
-
-public interface IProgressReporter
-{
-    Task ReportProgress(string phase, string message, int percentComplete);
-    Task ReportComplete(bool success, string message);
-    Task ReportError(string message);
-}
